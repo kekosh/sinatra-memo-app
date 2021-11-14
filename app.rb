51ca477b0c725830680049c2e1d9d8b7598c74f6
kekosh <kenta0540@gmail.com>
2021-11-14 23:16:55 +0900

    新規メモデータ登録処理作成開始

--- app.rb
+++ app.rb
@@ -1,6 +1,22 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'securerandom'
 
 get '/index' do
-  'test'
+  erb :index
+end
+
+get '/new' do
+  erb :new
+end
+
+post '/regist' do
+  @title = params['input_title']
+  @content = params['input_content']
+  @id = SecureRandom.uuid
+  @registered_at = Time.now.strftime("%Y%m%d%T")
+
+  p @registered_at
+
+  redirect to("/index")
 end
