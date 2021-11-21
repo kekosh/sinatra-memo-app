8649b46cb41e7b5d78dbb0acb113c9703959bd8a
kekosh <kenta0540@gmail.com>
2021-11-21 21:32:34 +0900

    新規メモデータ登録処理実装

--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
 require 'securerandom'
 require 'json'
+
+helpers do
+  def read_data(file)
+    File.open(file, 'r') do |f|
+      JSON.parse(f.read)
+    end
+  end
+end
 
 get '/index' do
   erb :index
@@ -15,14 +25,23 @@ post '/regist' do
   @title = params['input_title']
   @content = params['input_content']
   @id = SecureRandom.uuid
-  @registered_at = Time.now.strftime("%Y%m%d%T")
+  @registered_at = Time.now.strftime('%Y-%m-%d %H:%M:%S')
 
-  data = JSON.dump({
+  new_data = {
     "id": @id,
     "title": @title,
     "content": @content,
     "registered_at": @registered_at
-  })
+  }
 
-  redirect to("/index")
+  begin
+    @data_list = read_data('data.json')
+  rescue JSON::ParserError
+    @data_list = []
+  end
+
+  @data_list.push(new_data)
+  File.open('data.json', 'w') { |file| JSON.dump(@data_list, file) }
+
+  redirect to('/index')
 end
