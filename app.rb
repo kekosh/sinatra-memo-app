e9f54bff1e387fbeb457d5390197ad43087a4aff
kekosh <kenta0540@gmail.com>
2021-12-26 23:16:51 +0900

    DB接続処理のメモ化対応（コネクション使い回し）実施

--- app.rb
+++ app.rb
@@ -7,21 +7,9 @@ require 'json'
 require 'Rack'
 require 'pg'
 
-configure do
-  enable :method_override
-end
-
-not_found do
-  erb :notfound
-end
-
-def db_connect
-  PG.connect(dbname: 'memodb')
-end
-
 # Database Process
 class Database
-  def select_memo_titles
+  def select_memo_titles(db_connect)
     data_list = []
     w_sql = 'SELECT id, title FROM memos ORDER BY registered_at'
     db_connect.exec(w_sql) do |result|
@@ -32,90 +20,101 @@ class Database
     data_list
   end
 
-  def insert_new_memo(id, title, contents, registered_at)
+  def insert_new_memo(db_connect, id, title, contents, registered_at)
     w_sql = 'INSERT INTO memos VALUES ($1, $2, $3, $4)'
     db_connect.exec(w_sql, [id, title, contents, registered_at])
   end
 
-  def select_memo(id)
+  def select_memo(db_connect, id)
     w_sql = 'SELECT id, title, contents FROM memos WHERE id = $1'
     db_connect.exec(w_sql, [id])
   end
 
-  def delete_memo(id)
+  def delete_memo(db_connect, id)
     w_sql = 'DELETE FROM memos WHERE id = $1'
     db_connect.exec(w_sql, [id])
   end
 
-  def update_memo(id, title, contents, registerd_at)
+  def update_memo(db_connect, id, title, contents, registerd_at)
     w_sql = 'UPDATE memos SET title = $1, contents = $2, registered_at = $3 WHERE id = $4'
     db_connect.exec(w_sql, [title, contents, registerd_at, id])
   end
 end
 
-helpers do
-  def escape_html(str)
-    Rack::Utils.escape_html(str)
+# Main Class
+class MyApp < Sinatra::Base
+  configure do
+    enable :method_override
   end
-end
 
-get '/' do
-  redirect to('/memos')
-end
-
-get '/memos' do
-  database = Database.new
-  @data_list = database.select_memo_titles
-  erb :index
-end
-
-get '/memos/new' do
-  erb :new
-end
-
-post '/memos' do
-  database = Database.new
-  id = SecureRandom.uuid
-  title = params['input_title']
-  content = params['input_content']
-  registered_at = Time.now.strftime('%Y-%m-%d %H:%M:%S')
-  database.insert_new_memo(id, title, content, registered_at)
-  redirect to('/memos')
-end
-
-get '/memos/:id' do
-  database = Database.new
-  memo = database.select_memo(params['id'])
-
-  if memo.none?
+  not_found do
     erb :notfound
-  else
+  end
+
+  helpers do
+    def escape_html(str)
+      Rack::Utils.escape_html(str)
+    end
+  end
+
+  db_conn ||= PG.connect(dbname: 'memodb')
+  database ||= Database.new
+
+  get '/' do
+    redirect to('/memos')
+  end
+
+  get '/memos' do
+    @data_list = database.select_memo_titles(db_conn)
+    erb :index
+  end
+
+  get '/memos/new' do
+    erb :new
+  end
+
+  post '/memos' do
+    id = SecureRandom.uuid
+    title = params['input_title']
+    content = params['input_content']
+    registered_at = Time.now.strftime('%Y-%m-%d %H:%M:%S')
+    database.insert_new_memo(db_conn, id, title, content, registered_at)
+    redirect to('/memos')
+  end
+
+  get '/memos/:id' do
+    memo = database.select_memo(db_conn, params['id'])
+
+    if memo.none?
+      erb :notfound
+    else
+      memo.each { |record| @memo = record }
+      erb :detail
+    end
+  end
+
+  delete '/memos/:id' do
+    database.delete_memo(db_conn, params['id'])
+    redirect to('/memos')
+  end
+
+  get '/memos/:id/edit' do
+    memo = database.select_memo(db_conn, params['id'])
     memo.each { |record| @memo = record }
-    erb :detail
+    erb :edit
+  end
+
+  patch '/memos/:id' do
+    database.update_memo(
+      db_conn,
+      params['id'],
+      params['edit_title'],
+      params['edit_content'],
+      Time.now.strftime('%Y-%m-%d %H:%M:%S')
+    )
+
+    redirect to('/memos')
   end
 end
 
-delete '/memos/:id' do
-  database = Database.new
-  database.delete_memo(params['id'])
-  redirect to('/memos')
-end
-
-get '/memos/:id/edit' do
-  database = Database.new
-  memo = database.select_memo(params['id'])
-  memo.each { |record| @memo = record }
-  erb :edit
-end
-
-patch '/memos/:id' do
-  database = Database.new
-  database.update_memo(
-    params['id'],
-    params['edit_title'],
-    params['edit_content'],
-    Time.now.strftime('%Y-%m-%d %H:%M:%S')
-  )
-
-  redirect to('/memos')
-end
+MyApp.run!
